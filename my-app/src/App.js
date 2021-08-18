01778b269f318e064f68f298f91fdbab302647f8
Виктория Светлова <vika@MacBook-Pro-Viktoria.local>
2021-08-19 01:40:50 +0300

    add edit and valid

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,11 +2,12 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import UsersTable from './UsersTable.js';
 import AddUserButton from './AddUserButton.js';
-import useModal from './useModal';
 import EditUserForm from './EditUserForm';
+import useModal from './useModal';
 
 function App() {
   const [users, setUsers] = useState([ {"name": "Carl", "email": "example@gmail.com", "city": "Rome" } ]);
+  const [editingUserIndex, setEditingUserIndex] = useState(null);
 
   const {isShowing, toggle} = useModal();
 
@@ -22,15 +23,21 @@ function App() {
     setUsers(temp);
   }
 
-  const editRow = (index, user) => {
-    isShowing={isShowing}
-    toggle={toggle}
-    let temp = [...users];
-    temp.splice(index, 1, user);
-    setUsers(temp);
-    // let temp = [...users];
-    // temp.push(user);
-    // setUsers(temp);
+  const editRow = (index) => {
+    setEditingUserIndex(index);
+    toggle();
+  }
+
+  const makeEditFormCallback = (index) => {
+    if (index == null || index < 0) { return (user) => { }; }
+    return (user) => {
+      let temp = [...users];
+
+      temp[index] = user;
+      setUsers(temp);
+      setEditingUserIndex(null);
+      toggle();
+    };
   }
 
   useEffect(() => {
@@ -42,8 +49,13 @@ function App() {
       <div className="App-container">
         <div className="wrapper">
           <AddUserButton handleChange={handleChange} />
-          <UsersTable users={users} deleteRow={deleteRow} editRow={toggle, editRow}/>
-          <EditUserForm />
+          <UsersTable users={users} deleteRow={deleteRow} editRow={editRow}/>
+          <EditUserForm 
+            isShowing={isShowing}
+            hide={toggle}
+            addEditedUser={makeEditFormCallback(editingUserIndex)}
+            user={editingUserIndex == null ? {} : users[editingUserIndex]}
+          />
         </div>
       </div>
     </div>
--- my-app/src/EditUserForm.js
+++ my-app/src/EditUserForm.js
@@ -1,49 +1,73 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
+
+const isValidEmail = (value) => {
+  // const regex = new RegExp('');
+  // let result = regex.test(value);
+  return value !== '';
+  // console.log(result);
+  // return result;
+}
 
 const getData = () => {
     let dataUser = {};
     let userNameElemValue = document.getElementsByClassName('userName')[0].value;
     dataUser["name"] = userNameElemValue;
     let emailElemValue = document.getElementsByClassName('email')[0].value;
+
     dataUser["email"] = emailElemValue;
+    
     let cityElemValue = document.getElementsByClassName('city')[0].value;
     dataUser["city"] = cityElemValue;
     return dataUser;
   }
 
-const EditUserForm = ({ isShowing, hide, addEditedUser }) => isShowing ? ReactDOM.createPortal(
-  <React.Fragment>
-    <div className="modal-overlay"/>
-    <div className="modal-wrapper" aria-modal aria-hidden tabIndex={-1} role="dialog">
-      <div className="modal">
-        <div className="modal-header">
-          <button type="button" className="modal-close-button" data-dismiss="modal" aria-label="Close" onClick={hide}>
-            <span aria-hidden="true">&times;</span>
-          </button>
+const EditUserForm = ({ isShowing, hide, addEditedUser, user }) => {
+  const [isValid, setIsValid] = useState(true);
+
+  const errorMessage = isValid
+    ? <div/>
+    : <div><span>Wrong email</span></div>
+  return isShowing ? ReactDOM.createPortal(
+    <React.Fragment>
+      <div className="modal-overlay"/>
+      <div className="modal-wrapper" aria-modal aria-hidden tabIndex={-1} role="dialog">
+        <div className="modal">
+          <div className="modal-header">
+            <button type="button" className="modal-close-button" data-dismiss="modal" aria-label="Close" onClick={hide}>
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div className="container">
+            <div>
+              <span>User name</span>
+              <input type="text" className="userName" defaultValue={user.name}/>
+            </div>
+            <div>
+              <span>Email</span>
+              <input type="text" className="email" defaultValue={user.email}/>
+            </div>
+            {errorMessage}
+            <div>
+              <span>City</span>
+              <input type="text" className="city" defaultValue={user.city}/>
+            </div>
+          </div>
+          <button className="save-btn" onClick={ () => {
+            let user = getData();
+            let userIsValid = isValidEmail(user.email)
+
+            setIsValid(userIsValid);
+
+            if (userIsValid) {
+              addEditedUser(user);
+              hide();
+            }
+          }}>Save</button>
         </div>
-        <div className="container">
-          <div>
-            <span>User name</span>
-            <input type="text" className="userName" />
-          </div>
-          <div>
-            <span>Email</span>
-            <input type="text" className="email" />
-          </div>
-          <div>
-            <span>City</span>
-            <input type="text" className="city" />
-          </div>
-        </div>
-        <button className="save-btn" onClick={ () => {
-          let user = getData();
-          addEditedUser(user);
-          hide();
-        }}>Save</button>
       </div>
-    </div>
-  </React.Fragment>, document.body
-) : null;
+    </React.Fragment>, document.body
+  ) : null;
+}
 
 export default EditUserForm;
